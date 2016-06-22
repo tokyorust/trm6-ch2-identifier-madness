e10bdfc734b370995b765299d5997b412b64e6a1
Simo Kinnunen <simo@shoqolate.com>
2016-06-23 03:56:28 +0900

    Simple solution.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,42 @@
 #![feature(plugin)]
 #![plugin(clippy)]
 
+use std::fmt;
+
+enum Identifier {
+    UUID(String),
+    Numeric(u64),
+}
+
+impl fmt::Display for Identifier {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Identifier::UUID(ref id) => write!(f, "{}", id),
+            Identifier::Numeric(ref id) => write!(f, "{}", id),
+        }
+    }
+}
+
+impl From<String> for Identifier {
+    fn from(id: String) -> Identifier {
+        Identifier::UUID(id)
+    }
+}
+
+impl From<u64> for Identifier {
+    fn from(id: u64) -> Identifier {
+        Identifier::Numeric(id)
+    }
+}
+
 struct Record {
-    id: u64,
+    id: Identifier,
 }
 
 impl Record {
-    fn new(id: u64) -> Record {
+    fn new<I: Into<Identifier>>(id: I) -> Record {
         Record {
-            id: id,
+            id: id.into(),
         }
     }
 }
