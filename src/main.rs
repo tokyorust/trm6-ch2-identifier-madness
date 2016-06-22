3aaae698fba42d43858c83a4c57a19418005f270
Simo Kinnunen <simo@shoqolate.com>
2016-06-23 04:33:04 +0900

    Make the identifier optional.

--- src/main.rs
+++ src/main.rs
@@ -36,33 +36,48 @@ impl From<u64> for Identifier {
 }
 
 struct Record {
-    id: Identifier,
+    id: Option<Identifier>,
 }
 
 impl Record {
-    fn new<I: Into<Identifier>>(id: I) -> Record {
+    fn new<I: Into<Identifier>>(id: Option<I>) -> Record {
         Record {
-            id: id.into(),
+            id: id.map(|v| v.into()),
+        }
+    }
+}
+
+impl fmt::Display for Record {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.id {
+            Some(ref id) => write!(f, "{}", id),
+            None => write!(f, "<NEW>"),
         }
     }
 }
 
 fn main() {
-    let rec = Record::new(1);
-    println!("We have record {}!", rec.id);
+    let rec = Record::new(Some(1));
+    println!("We have record {}!", rec);
 }
 
 #[test]
 fn should_accept_u64() {
-    Record::new(0 as u64);
+    Record::new(Some(0 as u64));
 }
 
 #[test]
 fn should_accept_uuid_string() {
-    Record::new("49a6319c-38a9-11e6-8ed5-97272550a380".to_owned());
+    Record::new(Some("49a6319c-38a9-11e6-8ed5-97272550a380".to_owned()));
 }
 
 #[test]
 fn should_accept_uuid_str() {
-    Record::new("49a6319c-38a9-11e6-8ed5-97272550a380");
+    Record::new(Some("49a6319c-38a9-11e6-8ed5-97272550a380"));
+}
+
+#[test]
+fn should_accept_none() {
+    Record::new::<String>(None);
+    Record::new::<u64>(None);
 }
